abec4ceb8cf08e683338489d2430871993072bca
chokopapashi <chokopapashi@gmail.com>
2017-11-20 11:34:20 +0900

    finish programing

--- src/main/scala/DirectoryLister.scala
+++ src/main/scala/DirectoryLister.scala
@@ -1,49 +1,33 @@
-
-:reset
-
-/**/object DirectoryLister {
-
 import java.io.File
 
 import scala.language.implicitConversions
 
-/* -------------------------------------------------------------------------- */
+case class DirectoryLister(root: File) {
+    require(root.isDirectory)
 
-val root = new File("C:/hirose_work/work/Test/Scala_work/DirectoryLister")
+    var files: List[File] = newFiles(root)
 
-val newFiles(dir: File) = {
-    require(dir.isDirectory)
-    dir.listFiles.sortWith((f1,f2) => f2.isDirectory)
-}
+    /* ---------------------------------------------------------------------- */
 
-var files = newFiles(root)
+    def newFiles(dir: File): List[File] = {
+        require(dir.isDirectory)
+        dir.listFiles.sortWith((f1,f2) => f2.isDirectory).toList
+    }
 
-/* -------------------------------------------------------------------------- */
+    /* ---------------------------------------------------------------------- */
 
-trait Node
-case class FileNode(f: File) extends Node
-case class DirNode(f: File) extends Node = {
-
-}
-
-implicit def file2Node(f: File): Node = 
-    if(f.isDirectory) DirNode(f) else FileNode(f)
-
-def nextNode(n: Node) = n
-
-/* -------------------------------------------------------------------------- */
-
-def nextFile: Option[File] = {
-    files match {
-        case Nil => None
-        case fh :: fs => {
-            val n = nextNode(fh)
+    def nextFile: Option[File] = {
+        println(files.toString)
+        files match {
+            case Nil => None
+            case fh :: fs => {
+                if(fh.isDirectory) {
+                    files = newFiles(fh) ++ fs
+                }
+                val ret = Some(files.head)
+                files = files.tail
+                ret
+            }
         }
     }
 }
-
-
-/**/}
-
-import DirectoryLister._
-
